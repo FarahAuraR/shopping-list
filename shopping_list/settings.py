26f2bf834674bbf68c1dfcb8db9fc6b7cc59e1ad
FarahAuraR <farah.rosadi@gmail.com>
2023-11-15 14:33:14 +0700

    fix tutorial 7

--- shopping_list/settings.py
+++ shopping_list/settings.py
@@ -38,8 +38,8 @@ ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
     'main',
-    'authentication'
-    'corsheaders'
+    'authentication',
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
